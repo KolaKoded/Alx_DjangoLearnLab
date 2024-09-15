83e44d811d4ff504fe1fc242fb49d20c36101930
Kolade Fakunle <fakunlekolade@gmail.com>
2024-09-15 21:50:36 +0100

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,13 +4,13 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home' ),
-    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 
 
-    path('register', views.register, name='register'),
-    path('profile', views.profile, name='profile'),
-    path('update-profile', views.update_profile, name='update_profile'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('update-profile/', views.update_profile, name='update_profile'),
 
     path('posts/', views.PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
